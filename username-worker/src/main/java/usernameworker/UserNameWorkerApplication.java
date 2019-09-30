11ed7e3e3d6768a4c55e32731d94825add943281
Tran Huy Anh <tranhuyanh267@gmail.com>
2019-09-30 17:58:18 +0700

    asd

--- username-worker/src/main/java/usernameworker/UserNameWorkerApplication.java
+++ username-worker/src/main/java/usernameworker/UserNameWorkerApplication.java
@@ -1,12 +1,23 @@
 package usernameworker;
 
+import lombok.extern.log4j.Log4j;
+import org.brunocvcunha.instagram4j.Instagram4j;
+import org.brunocvcunha.instagram4j.requests.InstagramSearchUsernameRequest;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramSearchUsernameResult;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.handler.annotation.SendTo;
 
+import java.io.IOException;
+
+@Log4j
 @SpringBootApplication
 public class UserNameWorkerApplication {
+
+    @Autowired
+    private Instagram4j instagram4j;
 
     public static void main(String[] args) {
         SpringApplication.run(UserNameWorkerApplication.class, args);
@@ -14,8 +25,16 @@ public class UserNameWorkerApplication {
 
     @RabbitListener(queues = "usernameQueue")
     @SendTo("userIdQueue")
-    public String handleUsername(String username) {
-        return username;
+    public long handleUsername(String username) {
+        try {
+            log.info("doing handleUsername " + username);
+            InstagramSearchUsernameRequest request = new InstagramSearchUsernameRequest(username);
+            InstagramSearchUsernameResult result = instagram4j.sendRequest(request);
+            return result.getUser().getPk();
+        } catch (IOException e) {
+            log.error("error handleUsername " + username);
+            return 0;
+        }
     }
 
 }
