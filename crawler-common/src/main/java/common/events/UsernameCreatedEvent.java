e3350c321857fb14a142556ad8e6b161acaae883
Tran Huy Anh <tranhuyanh267@gmail.com>
2019-10-15 12:35:02 +0700

    add crawTime

--- crawler-common/src/main/java/common/events/UsernameCreatedEvent.java
+++ crawler-common/src/main/java/common/events/UsernameCreatedEvent.java
@@ -1,16 +1,20 @@
 package common.events;
 
+import java.util.Date;
+
 public class UsernameCreatedEvent extends Event {
     private String username;
     private String category;
+    private Date crawTime;
 
     public UsernameCreatedEvent() {
 
     }
 
-    public UsernameCreatedEvent(String username, String category) {
+    public UsernameCreatedEvent(String username, String category, Date crawTime) {
         this.username = username;
         this.category = category;
+        this.crawTime = crawTime;
     }
 
     public String getUsername() {
@@ -27,5 +31,13 @@ public class UsernameCreatedEvent extends Event {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public Date getCrawTime() {
+        return crawTime;
+    }
+
+    public void setCrawTime(Date crawTime) {
+        this.crawTime = crawTime;
     }
 }
